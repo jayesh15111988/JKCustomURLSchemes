b3fb38f469f52d358e80987593d4d1ce95f85dbb
Jayesh Kawli <j.kawli@gmail.com>
2016-05-02 20:39:45 -0400

    Initial project setup

---
+++ JKCustomURLSchemes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JKCustomURLSchemes
+//
+//  Created by Jayesh Kawli Backup on 5/2/16.
+//  Copyright © 2016 Jayesh Kawli Backup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
